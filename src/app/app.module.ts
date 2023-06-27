e1f84e04d93ea442fd9c28aa6bcd26807dbe461b
cidaluna <cidalunna@hotmail.com>
2023-06-27 20:05:32 -0300

    Created stub component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { NgModule } from '@angular/core';
+import { ApplicationModule, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConceptsTestsComponent } from './concepts-tests/concepts-tests.component';
+import { StubComponent } from './stub/stub.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConceptsTestsComponent
+    ConceptsTestsComponent,
+    StubComponent
   ],
   imports: [
+    CommonModule,
+    ApplicationModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
